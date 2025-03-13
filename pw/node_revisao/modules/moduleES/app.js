1f9e9757025f3b0e990f348d5672bed1619b31b1
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-03-13 14:34:55 -0300

    coisas atrasadas

--- pw/node_revisao/modules/moduleES/app.js
+++ pw/node_revisao/modules/moduleES/app.js
@@ -1,3 +1,13 @@
-import factorial from "./module_b.js";
+import factorial from './module_b.js';
+import {sum, div} from './calc.js'; 
+import getStudents from './module_d.js';
 
-console.log(factorial(10));
+console.log(sum(factorial(5), div(factorial(6), 100)));
+console.log(getStudents());
+
+getStudents().map(({ nome }) => console.log(nome));
+getStudents()
+    .filter(({ situacao }) => situacao == "real")
+    .map(({ nome }) => console.log(nome));
+
+//mlem :3
---
+++ pw/node_revisao/modules/moduleES/module_d.js
@@ -0,0 +1,8 @@
+const students = [
+    { 'rm': 1, 'nome': 'Luna', 'media': 9.9, 'situacao': 'real' },
+    { 'rm': 1, 'nome': 'Jomp', 'media': 2.2, 'situacao': 'falso' },
+    { 'rm': 1, 'nome': 'Fofinha', 'media': 10, 'situacao': 'real' },
+    { 'rm': 1, 'nome': 'algo', 'media': 7, 'situacao': 'real' }
+]
+
+export default () => students;
