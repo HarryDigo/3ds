dfa8eac3df5e2e84c69cbeb82d70227d5d8f9d9c
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-08-13 08:42:05 -0300

    ainda nao acabei o home

--- pw/react_3/react-router/src/components/MainCounter/MainCounter.jsx
+++ pw/react_3/react-router/src/components/MainCounter/MainCounter.jsx
@@ -1,13 +1,13 @@
 import { UseCounterContext } from '../../hooks'
 
-import './mainCounter.module.css'
+import styles from './mainCounter.module.css'
 
 const MainCounter = () => {
   const { count } = UseCounterContext()
 
   return (
     //não tenho a minima ideia do porque, mas a fonte só funcionou no inline e não no style ????
-    <span style={{fontFamily: "Comic Sans MS, cursive, sans-serif" }}>
+    <span className={styles.funny} style={{fontFamily: "Comic Sans MS, cursive, sans-serif" }}>
       {count}
     </span>
   );
--- pw/react_3/react-router/src/pages/Home.jsx
+++ pw/react_3/react-router/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 const Home = () => {
   return (
     <>
-      
+      <h1>Bem vindo ao site! :3</h1>
+
+      <p>Compliquei o projeto um pouco mais do que o esperado, mas ainda acho que está bom</p>
+
+      <h2>Anúncio importante (de mim)</h2>
+
+      <p>
+        Com o início das aulas do Gustavo e o susto absoluto que ele deu na primeira
+        <br />
+        É importante dizer que estou passando por uns desenvolvimentos pessoais faz um bom tempo
+        <br />
+        Estou ficando mais comfortável com isso e já está na etapa de espalhar para a sala
+        <br /><br />
+        aaaaaaaaaaaaa
+      </p>
     </>
   )
 }
