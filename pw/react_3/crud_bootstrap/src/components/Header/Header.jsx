e0f57804a459e3cb9c90839cbddb5e41deaeab3f
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-09-02 15:44:09 -0300

    tarefa de crud e react-bootstrap

---
+++ pw/react_3/crud_bootstrap/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand>:3 products</Navbar.Brand>
+      </Container>
+    </Navbar>
+  )
+}
+
+export { Header }
