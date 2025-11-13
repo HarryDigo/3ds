84690e65dc0bb2dbd8e1e7a2f1e7915e5649e7a9
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-11-13 08:26:52 -0300

    mapa bÃasico adicionado

--- pam/native_4/maps-pam/src/App.js
+++ pam/native_4/maps-pam/src/App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -23.55052,
+          longitude: -46.633308,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      >
+        <Marker
+          coordinate={{latitude: -23.55052, longitude: -46.633308}}
+          title={"São Paulo"}
+          description={"Capital financeira do Brasil"}
+        />
+      </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
